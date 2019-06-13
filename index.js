b6ab9fe43696f24812a02d78714413897f82ba99
cgao <cai.gao@icloud.com>
2019-06-13 08:29:08 -0400

    Create a store that stores state

---
+++ index.js
@@ -0,0 +1,25 @@
+function createStore () {
+  let state
+  let listeners = []
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+  }
+
+  return {
+    getState,
+    subscribe
+  }
+}
+
+const store = createStore()
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+store.subscribe(() => {
+  console.log('The store changed', store.getState())
+})
