13103a346d2439a114cae83c3b302692e34a288d
cgao <cai.gao@icloud.com>
2019-06-17 07:52:08 -0400

    Function to keep track goals

--- index.js
+++ index.js
@@ -47,6 +47,18 @@ function todos (state = [], action) {
   }
 }
 
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
+}
+
+
 // example
 const store = createStore(todos)
 
